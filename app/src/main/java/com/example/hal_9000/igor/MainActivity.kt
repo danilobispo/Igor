b5483fdfeb6becd6d62756350990d67ec7076bc4
danilobispo <danilobispogalvao@gmail.com>
2018-09-20 16:21:17 -0300

    Correção de erros

--- app/src/main/java/com/example/hal_9000/igor/MainActivity.kt
+++ app/src/main/java/com/example/hal_9000/igor/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupMenu()
-        val listaDeCategorias: ArrayList<Categoria> = montarListaDeOpcoes(this)
+//        val listaDeCategorias: ArrayList<Categoria> = montarListaDeOpcoes(this)
 
 //        expandableListView.setAdapter()
         setupFragment(savedInstanceState)
