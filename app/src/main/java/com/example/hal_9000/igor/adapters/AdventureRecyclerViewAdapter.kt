ca0befdd9417beba5e1e97d46d2a53b5c883d7e4
Matheus Vieira <matheusov@gmail.com>
2018-10-13 12:16:29 -0300

    Home edit mode

--- app/src/main/java/com/example/hal_9000/igor/adapters/AdventureRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/hal_9000/igor/adapters/AdventureRecyclerViewAdapter.kt
@@ -12,10 +12,11 @@ import com.example.hal_9000.igor.R
 import com.example.hal_9000.igor.model.Aventura
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestoreException
 
 
-class AdventureRecyclerViewAdapter(options: FirestoreRecyclerOptions<Aventura>) : FirestoreRecyclerAdapter<Aventura, AdventureRecyclerViewAdapter.AdventureViewHolder>(options) {
+class AdventureRecyclerViewAdapter(options: FirestoreRecyclerOptions<Aventura>, private val editMode: Boolean, private val itemClickListener: (Aventura) -> Unit, private val deleteClickListener: (Aventura) -> Unit) : FirestoreRecyclerAdapter<Aventura, AdventureRecyclerViewAdapter.AdventureViewHolder>(options) {
 
     private val TAG = "AdventureRecyclrAdapter"
 
@@ -28,49 +29,72 @@ class AdventureRecyclerViewAdapter(options: FirestoreRecyclerOptions<Aventura>) 
         holder.setAdventureTitle(model.title)
         holder.setAdventureNextSession(model.next_session)
         holder.setAdventureTheme(model.theme)
+
+        if (editMode)
+            holder.setDeleteClickListener(model, deleteClickListener)
+        else
+            holder.setClickListener(model, itemClickListener)
     }
 
     override fun onError(e: FirebaseFirestoreException) {
         Log.e(TAG, "Error: $e.message")
     }
 
-    class AdventureViewHolder internal constructor(private val view: View) : RecyclerView.ViewHolder(view) {
+    class AdventureViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun setAdventureTitle(adventureTitle: String) {
-            val title: TextView = view.findViewById(R.id.tv_title)
+            val title: TextView = itemView.findViewById(R.id.tv_title)
             title.text = adventureTitle
         }
 
         fun setAdventureNextSession(adventureNextSession: String) {
-            val nextSession: TextView = view.findViewById(R.id.tv_next_session)
+            val nextSession: TextView = itemView.findViewById(R.id.tv_next_session)
             val nextSessionText = "próxima sessão $adventureNextSession"
             nextSession.text = nextSessionText
         }
 
         fun setAdventureTheme(adventureTheme: String) {
-            val image: ImageView = view.findViewById(R.id.iv_theme)
+            val image: ImageView = itemView.findViewById(R.id.iv_theme)
 
             when (adventureTheme) {
                 "krevast" ->
-                    Glide.with(view)
+                    Glide.with(itemView)
                             .load(R.drawable.miniatura_krevast)
                             .into(image)
                 "corvali" ->
-                    Glide.with(view)
+                    Glide.with(itemView)
                             .load(R.drawable.miniatura_corvali)
                             .into(image)
                 "heartlands" ->
-                    Glide.with(view)
+                    Glide.with(itemView)
                             .load(R.drawable.miniatura_heartlands)
                             .into(image)
                 "coast" ->
-                    Glide.with(view)
+                    Glide.with(itemView)
                             .load(R.drawable.miniatura_coast)
                             .into(image)
                 else ->
-                    Glide.with(view)
+                    Glide.with(itemView)
                             .load(R.drawable.miniatura_imagem_automatica)
                             .into(image)
             }
+        }
+
+        fun setClickListener(aventura: Aventura, clickListener: (Aventura) -> Unit) {
+            itemView.setOnClickListener { clickListener(aventura) }
+        }
+
+        fun setDeleteClickListener(aventura: Aventura, clickListener: (Aventura) -> Unit) {
+
+            val mAuth = FirebaseAuth.getInstance()
+            if (aventura.creator != mAuth.uid) {
+                val overlay: View = itemView.findViewById(R.id.overlay_image)
+                overlay.visibility = View.VISIBLE
+                return
+            }
+
+            val image: ImageView = itemView.findViewById(R.id.iv_delete)
+            image.visibility = View.VISIBLE
+            image.setOnClickListener { clickListener(aventura) }
         }
     }
 }
--- app/src/main/java/com/example/hal_9000/igor/fragment/AventuraFragment.kt
+++ app/src/main/java/com/example/hal_9000/igor/fragment/AventuraFragment.kt
@@ -4,18 +4,22 @@ package com.example.hal_9000.igor.fragment
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.Fragment
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.hal_9000.igor.R
 import com.example.hal_9000.igor.adapters.AdventureRecyclerViewAdapter
 import com.example.hal_9000.igor.model.Aventura
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_aventura.*
 
 
 class AventuraFragment : Fragment() {
@@ -23,6 +27,8 @@ class AventuraFragment : Fragment() {
     private val TAG = "AventuraFragment"
 
     private var adapter: AdventureRecyclerViewAdapter? = null
+    private var mRecyclerView: RecyclerView? = null
+    private var db: FirebaseFirestore? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -31,26 +37,151 @@ class AventuraFragment : Fragment() {
 
         Log.d(TAG, "onCreateView: Started")
 
+        setHasOptionsMenu(true)
 
-        val mRecyclerView: RecyclerView = view.findViewById(R.id.rv_adventures_list)
-        mRecyclerView.layoutManager = LinearLayoutManager(context)
+        mRecyclerView = view.findViewById(R.id.rv_adventures_list)
+        mRecyclerView?.layoutManager = LinearLayoutManager(context)
 
-        val db = FirebaseFirestore.getInstance()
-        val query = db.collection("adventures")
+        db = FirebaseFirestore.getInstance()
+        val query = db!!.collection("adventures").whereEqualTo("deleted", false)
 
         val options = FirestoreRecyclerOptions.Builder<Aventura>()
                 .setQuery(query, Aventura::class.java)
                 .build()
 
-        adapter = AdventureRecyclerViewAdapter(options)
-        mRecyclerView.adapter = adapter
+        adapter = AdventureRecyclerViewAdapter(options, false, { aventura: Aventura -> aventuraItemClicked(aventura) }, { aventura: Aventura -> aventuraDeleteItemClicked(aventura) })
+        mRecyclerView?.adapter = adapter
 
         val fab = view.findViewById<FloatingActionButton>(R.id.fab_nova_aventura)
         fab.setOnClickListener {
             findNavController(this).navigate(R.id.action_aventuraFragment_to_newAdventure)
         }
 
+        val fabEditMode = view.findViewById<FloatingActionButton>(R.id.fab_edit_mode)
+        fabEditMode.setOnClickListener {
+            setEditModeOff()
+        }
+
+        val fabEditSave = view.findViewById<FloatingActionButton>(R.id.fab_save_edit)
+        fabEditSave.setOnClickListener {
+            setEditModeOff()
+        }
+
         return view
+    }
+
+    private fun aventuraItemClicked(aventura: Aventura) {
+        Toast.makeText(context, "Clicked: ${aventura.title}", Toast.LENGTH_LONG).show()
+    }
+
+    private fun aventuraDeleteItemClicked(aventura: Aventura) {
+        AlertDialog.Builder(view!!.context)
+                .setTitle("Deletar aventura")
+                .setMessage("Tem certeza que deseja deletar a aventura  ${aventura.title}?")
+                .setPositiveButton("Deletar") { _, _ ->
+//                    deleteAdventure(aventura)
+                    hideAdventure(aventura)
+                }
+                .setNegativeButton("Cancelar") { _, _ ->
+                    Toast.makeText(context, "Acão cancelada", Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, "aventuraDeleteItemClicked: aborted")
+                }
+                .show()
+    }
+
+    private fun deleteAdventure(aventura: Aventura) {
+        Log.d(TAG, "Deleting adventure ${aventura.title}")
+
+        db!!.collection("adventures")
+                .whereEqualTo("title", aventura.title)
+                .whereEqualTo("creator", aventura.creator)
+                .limit(1)
+                .get()
+                .addOnSuccessListener { it ->
+                    it.documents[0].reference
+                            .delete()
+                            .addOnSuccessListener { it2 ->
+                                Log.d(TAG, "DocumentSnapshot successfully deleted!")
+                                Toast.makeText(context, "Aventura deletada", Toast.LENGTH_SHORT).show()
+                            }
+                            .addOnFailureListener { e ->
+                                Log.w(TAG, "Error deleting document", e)
+                                Toast.makeText(context, "Erro ao deletar aventura", Toast.LENGTH_SHORT).show()
+                            }
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error querying document", e)
+                    Toast.makeText(context, "Erro ao deletar aventura", Toast.LENGTH_SHORT).show()
+                }
+    }
+
+    private fun hideAdventure(aventura: Aventura) {
+        Log.d(TAG, "Hiding adventure ${aventura.title}")
+
+        db!!.collection("adventures")
+                .whereEqualTo("title", aventura.title)
+                .whereEqualTo("creator", aventura.creator)
+                .limit(1)
+                .get()
+                .addOnSuccessListener { it ->
+                    it.documents[0].reference
+                            .update("deleted", true)
+                            .addOnSuccessListener { _ ->
+                                Log.d(TAG, "DocumentSnapshot successfully updated!")
+                                Toast.makeText(context, "Aventura deletada", Toast.LENGTH_SHORT).show()
+                            }
+                            .addOnFailureListener { e ->
+                                Log.w(TAG, "Error updating document", e)
+                                Toast.makeText(context, "Erro ao deletar aventura", Toast.LENGTH_SHORT).show()
+                            }
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error querying document", e)
+                    Toast.makeText(context, "Erro ao deletar aventura", Toast.LENGTH_SHORT).show()
+                }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_editar -> {
+                Toast.makeText(context, "Editar", Toast.LENGTH_SHORT).show()
+                setEditModeOn()
+            }
+            R.id.menu_ordenar -> {
+                Toast.makeText(context, "Ordenar", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setRecyclerView(editMode: Boolean) {
+        val query = db!!.collection("adventures").whereEqualTo("deleted", false)
+
+        val options = FirestoreRecyclerOptions.Builder<Aventura>()
+                .setQuery(query, Aventura::class.java)
+                .build()
+
+        adapter!!.stopListening()
+        adapter = AdventureRecyclerViewAdapter(options, editMode, { aventura: Aventura -> aventuraItemClicked(aventura) }, { aventura: Aventura -> aventuraDeleteItemClicked(aventura) })
+        mRecyclerView?.adapter = adapter
+        adapter!!.stopListening()
+        adapter!!.startListening()
+    }
+
+    private fun setEditModeOn() {
+        Log.d(TAG, "Edit mode on")
+        fab_nova_aventura.hide()
+        fab_save_edit.show()
+        fab_edit_mode.show()
+        setRecyclerView(true)
+    }
+
+    private fun setEditModeOff() {
+        Log.d(TAG, "Edit mode off")
+        fab_save_edit.hide()
+        fab_edit_mode.hide()
+        fab_nova_aventura.show()
+        setRecyclerView(false)
     }
 
     override fun onStart() {
